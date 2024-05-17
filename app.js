9bfb3fd6e4952aadc101495fa2c069634ef0381d
fausto <faustoquispeguzman@gmail.com>
2024-05-17 18:41:39 -0500

    modificacion fausto salida

--- app.js
+++ app.js
@@ -1,87 +1,49 @@
-//HANDLEBARS
-const express = require('express')
-const hbs = require('hbs');
 require('dotenv').config();
-const app = express()
+const express = require('express');
+const hbs = require('hbs');
+const app = express();
 const port = process.env.PORT;
 
-app.set('view engine', 'hbs');
+// Handlebars setup
+app.set('view engine', 'hbs'); // para usar Handlebars para Express
+hbs.registerPartials(__dirname + '/views/partials'); // para usar parciales
 
-
-app.get('/',(req, res) => {
-    res.render('home',{
-        nombre:'GRUPO NODE',
-        titulo:'Curso de Node'
-   });
-})
-hbs.registerPartials(__dirname + '/views/partials');
-app.use(express.static('public'));
-
-app.get('/elements', (req, res) => {
-    res.render('elements', {
-        nombre: 'GRUPO NODE',
-        titulo: 'Curso de Node'
-    });
-})
-
-app.get('/generic', (req, res) => {
-    res.render('generic', {
-        nombre: 'GRUPO NODE',
-        titulo: 'Curso de Node'
-    });
-})
-
-app.get('*', (req, res)=> {
-  res.sendFile(__dirname +'/public/404.html');
-})
-
-app.listen(port, () => {
-    console.log(`Example app listenining al http://localhost:${port}`)
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-const express = require('express')
-const app = express()
-const port = 8080;
-app.use(express.static('public'));
-//app.get('/', (req, res) => {
-//res.send('Home Page')
-//})
-
-app.get('/hola-mundo', (req, res)=> {
-res.send('Hola Mundo en su respectiva ruta')
-})
-
-//app.get('*', (req, res)=> {
-//    res.sendFile(__dirname +'/public/404.html');
-//})
-
-app.listen(port, () => {
-    console.log(`Example app listenining al http://localhost:${port}`)
-})
-
-app.get('/generic', (req, res) => {
-res.sendFile(__dirname + '/public/generic.html')
+// Rutas
+app.get('/', (req, res) => {
+  res.render('home', { // estos son los argumentos
+    nombre: 'Armando David',
+    titulo: 'Curso de Node'
+  });
 });
 
 app.get('/elements', (req, res) => {
-    res.sendFile(__dirname + '/public/elements.html')
+  res.render('elements', {
+    nombre: 'Armando David',
+    titulo: 'Curso de Node'
+  });
 });
-*/
+
+app.get('/generic', (req, res) => {
+  res.render('generic', {
+    nombre: 'Armando David',
+    titulo: 'Curso de Node'
+  });
+});
+
+app.get('/salidas', (req, res) => {
+  res.render('salidas', {
+    nombre: 'Armando David Espinoza',
+    titulo: 'Curso de Node'
+  });
+});
+
+// Servir contenido estático
+app.use(express.static('public')); // definición de middleware Express. Si 'public' está en otro sitio, especificar path
+
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/public/404.html');
+});
+
+app.listen(port, () => {
+  console.log("Example app listening at http://localhost:${port}");
+});
